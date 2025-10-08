a2c69fe62be83609bd4c91ee18a5b28604bb5bbf
kawakami-masaki0 <kawakami.masaki@jp.panasonic.com>
2025-10-08 12:11:00 +0900

    Update fw version referenced in setup.py (#72)

--- edgemdt_cl/__init__.py
+++ edgemdt_cl/__init__.py
@@ -18,14 +18,14 @@
 # library names are the names that are used in import statement rather than pip package name, as a library can have
 # multiple providing packages per arch, device etc.
 required_libraries = {
-    'tf': ['tensorflow>=2.10'],
-    'torch': ['torch>=2.0', 'torchvision>=0.15'],
-    'torch_ort': ['onnx', 'onnxruntime', 'onnxruntime_extensions>=0.8.0'],
+    'tf': ['tensorflow>=2.14'],
+    'torch': ['torch>=2.3', 'torchvision>=0.18'],
+    'torch_ort': ['onnx>=1.14', 'onnxruntime>=1.15', 'onnxruntime_extensions>=0.8.0'],
 }
 
 # pinned requirements of latest tested versions for extra_requires
 pinned_pip_requirements = {
     'tf': ['tensorflow==2.15.*'],
-    'torch': ['torch==2.5.*', 'torchvision==0.20.*'],
+    'torch': ['torch==2.6.*', 'torchvision==0.21.*'],
     'torch_ort': ['onnx==1.17.*', 'onnxruntime==1.21.*', 'onnxruntime_extensions==0.13.*']
 }
