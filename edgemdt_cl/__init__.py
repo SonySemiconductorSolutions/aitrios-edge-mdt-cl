ef506665317e1c8a6dc225480b5dfdc5107c8025
ueno-hiroshi002 <ueno.hiroshi002@jp.panasonic.com>
2025-10-14 19:25:10 +0900

    switch required onnxruntime version(<1.20) if python3.9

--- edgemdt_cl/__init__.py
+++ edgemdt_cl/__init__.py
@@ -13,19 +13,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # -----------------------------------------------------------------------------
+import sys
 
 # minimal requirements for dynamic validation in edgemdt_cl.{keras, pytorch}.__init__
 # library names are the names that are used in import statement rather than pip package name, as a library can have
 # multiple providing packages per arch, device etc.
-required_libraries = {
-    'tf': ['tensorflow>=2.14'],
-    'torch': ['torch>=2.3', 'torchvision>=0.18'],
-    'torch_ort': ['onnx>=1.14', 'onnxruntime>=1.15', 'onnxruntime_extensions>=0.8.0'],
-}
+if sys.version_info.major == 3 and sys.version_info.minor == 9:
+    # onnxruntime<1.20 if python3.9
+    required_libraries = {
+        'tf': ['tensorflow>=2.14'],
+        'torch': ['torch>=2.3', 'torchvision>=0.18'],
+        'torch_ort': ['onnx>=1.14', 'onnxruntime>=1.15,<1.20', 'onnxruntime_extensions>=0.8.0'],
+    }
+else:
+    required_libraries = {
+        'tf': ['tensorflow>=2.14'],
+        'torch': ['torch>=2.3', 'torchvision>=0.18'],
+        'torch_ort': ['onnx>=1.14', 'onnxruntime>=1.15', 'onnxruntime_extensions>=0.8.0'],
+    }
 
 # pinned requirements of latest tested versions for extra_requires
-pinned_pip_requirements = {
-    'tf': ['tensorflow==2.15.*'],
-    'torch': ['torch==2.6.*', 'torchvision==0.21.*'],
-    'torch_ort': ['onnx==1.17.*', 'onnxruntime==1.21.*', 'onnxruntime_extensions==0.13.*']
-}
+if sys.version_info.major == 3 and sys.version_info.minor == 9:
+    # onnxruntime<1.20 if python3.9
+    pinned_pip_requirements = {
+        'tf': ['tensorflow==2.15.*'],
+        'torch': ['torch==2.6.*', 'torchvision==0.21.*'],
+        'torch_ort': ['onnx==1.17.*', 'onnxruntime==1.19.*', 'onnxruntime_extensions==0.13.*']
+else:
+    pinned_pip_requirements = {
+        'tf': ['tensorflow==2.15.*'],
+        'torch': ['torch==2.6.*', 'torchvision==0.21.*'],
+        'torch_ort': ['onnx==1.17.*', 'onnxruntime==1.21.*', 'onnxruntime_extensions==0.13.*']
+    }
